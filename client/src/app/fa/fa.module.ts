2b906827ab1da9a33838fa5c3dafff831214bcf0
Edwin Svensson <git@olback.net>
2019-04-22 21:54:07 +0200

    minor details

--- client/src/app/fa/fa.module.ts
+++ client/src/app/fa/fa.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 // import { } from '@fortawesome/free-regular-svg-icons';
+import { } from '@fortawesome/free-brands-svg-icons';
 import {
   faCrown,
   faUser,
@@ -11,6 +12,11 @@ import {
   faClone,
   faCog
 } from '@fortawesome/free-solid-svg-icons';
+
+import {
+  faGithub,
+  faTwitter
+} from '@fortawesome/free-brands-svg-icons';
 
 @NgModule({
   imports: [FontAwesomeModule],
@@ -25,7 +31,9 @@ export class FAModule {
       faFlagCheckered,
       faGavel,
       faClone,
-      faCog
+      faCog,
+      faGithub,
+      faTwitter
     );
   }
 }
