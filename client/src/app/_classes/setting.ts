e4dea7c855dafe54fce7a26e3643fd3134ddb35c
Edwin Svensson <git@olback.net>
2019-05-16 00:55:08 +0200

    Added functionality to settings

--- client/src/app/_classes/setting.ts
+++ client/src/app/_classes/setting.ts
@@ -3,30 +3,54 @@ export class Setting {
   constructor(
       public name: string,
       public type: 'String' | 'Number' | 'Boolean',
-      public prettyName = '',
+      public prettyName: string,
+      public defaultValue: any,
+      public description?: string,
       private cb?: (v?: any) => void
     ) { }
 
   public set(value: any) {
-    localStorage.setItem(this.name, String(value));
-    if (this.cb) {
+
+    if (this.type === 'Number') {
+
+      const n = Number(value);
+      localStorage.setItem(this.name, String(isNaN(n) ? this.defaultValue : n));
+      if (this.cb) {
         this.cb(value);
+      }
+
+    } else {
+
+      localStorage.setItem(this.name, String(value));
+      if (this.cb) {
+          this.cb(value);
+      }
+
     }
+
   }
 
   public get() {
 
     const data = localStorage.getItem(this.name);
 
-    if (this.type === 'Boolean') {
+    if (data) {
 
-      return data === 'true';
+      if (this.type === 'Boolean') {
+
+        return data === 'true';
+
+      } else {
+
+        // This is ok because this.type will always be "String", "Number" or "Boolean".
+        // @ts-ignore
+        return window[this.type](data);
+
+      }
 
     } else {
 
-      // This is ok because this.type will always be "String", "Number" or "Boolean".
-      // @ts-ignore
-      return window[this.type](data);
+      return this.defaultValue;
 
     }
 
--- client/src/app/_services/settings.service.ts
+++ client/src/app/_services/settings.service.ts
@@ -3,6 +3,9 @@ import { Setting } from '../_classes/setting';
 import { EasterEggService } from './easter-egg.service';
 
 interface ISettings {
+  acronyms: Setting;
+  eggs: Setting;
+  scrollFactor: Setting;
   [key: string]: Setting;
 }
 
@@ -14,10 +17,11 @@ export class SettingsService {
   constructor(private _eggs: EasterEggService) {}
 
   public settings: ISettings = {
-    acronyms: new Setting('acronyms', 'Boolean', 'Enable random acronyms (offensive)'),
-    eggs: new Setting('eggs', 'Boolean', 'Enable easter eggs', () => {
+    acronyms: new Setting('acronyms', 'Boolean', 'Enable random acronyms', false, 'May be offensive.'),
+    eggs: new Setting('eggs', 'Boolean', 'Enable easter eggs', true, 'You\'re a boring human if you disable this.', () => {
       this._eggs.disableAll();
-    })
+    }),
+    scrollFactor: new Setting('scrollFactor', 'Number', 'Scroll speed', 1, 'Change this to scroll through cards faster or slower. Negative values changes the direction.')
   };
 
 }
